59e6800298af9eefb8b1daec549932e1d512361f
Windows10 Pro <gadongkarl2@gmail.com>
2021-04-05 17:37:47 +0800

    Navigation with Fragments

--- app/src/main/java/com/android/example/pickreci/MainActivity.kt
+++ app/src/main/java/com/android/example/pickreci/MainActivity.kt
@@ -2,32 +2,88 @@ package com.android.example.pickreci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    lateinit var navView: NavigationView
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        navController = Navigation.findNavController(this, R.id.fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
 
         navView = findViewById(R.id.nav_view)
-        NavigationUI.setupWithNavController(nav_view,navController)
-        NavigationUI.setupActionBarWithNavController(this,navController,drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_home, R.id.nav_myaccount,  R.id.nav_recipe, R.id.nav_orders, R.id.nav_cart, R.id.nav_aboutus), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
 
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_home -> {
+                Toast.makeText(this, "Home Clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_myaccount -> {
+                Toast.makeText(this, "My Account Clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_recipe -> {
+                Toast.makeText(this, "Recipe Clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_orders -> {
+                Toast.makeText(this, "Orders Clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_cart -> {
+                Toast.makeText(this, "Cart Clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_aboutus -> {
+                Toast.makeText(this, "About Us Clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
 }
+
